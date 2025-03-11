35cced6d173c550845ff894d6ad75bea9a60b978
saeed <s7fantastic@gmail.com>
2025-03-12 00:21:06 +0330

    Bug Fixed Config.js

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ import { getMongodbUrl } from '../config/database.js'
 
 const config = {
     database: getMongodbUrl,
-    production: false,
+    production: true,
     productionUrl: "https://honardooz.com",
     developmentCorsUrl: ["http://127.0.0.1:3000","http://127.0.0.1:8081","http://localhost:8081"],
     domain: "honardooz.com",
