1f1f207a125ecf6cc05c24ec07315b20f2c62628
saeed <s7fantastic@gmail.com>
2025-03-12 02:47:26 +0330

    Deploy

--- config/index.js
+++ config/index.js
@@ -3,7 +3,7 @@ import { getMongodbUrl } from '../config/database.js'
 const config = {
     database: getMongodbUrl,
     production: true,
-    productionUrl: "http://127.0.0.1:3000",
+    productionUrl: "https://lead-flow-front-end-9lwt.vercel.app",
     developmentCorsUrl: ["http://127.0.0.1:3000","http://127.0.0.1:8081","http://localhost:8081"],
 
 }
